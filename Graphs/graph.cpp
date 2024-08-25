fc18610523d7b4de248eb9ceaee9ab3b11abf972
Gaurav Shah <shahg152002@gmail.com>
2024-08-25 10:48:36 +0530

    Changes in graph c++ code

--- Graphs/graph.cpp
+++ Graphs/graph.cpp
@@ -4,81 +4,28 @@
 #include <queue>
 using namespace std;
 
-
-/*
-dfs and bfs
- */
-
-// dfs 
-
-// class Solution {
-//     public : 
-//         vector<int> bfsGraphTraversal(int V, vector<pair<int, int>> adj){
-//             vector<vector<int>> adjList(V, vector<int>(V));
-//             for(auto it : adj){
-//                 adjList[it.first].push_back(it.second);
-//                 adjList[it.second].push_back(it.first);
-//             }
-//             vector<int> vis = {0};
-//             vis[0] = 1;
-//             queue<int> q;
-//             q.push(0);
-//             vector<int> bfs;
-
-//             while(!q.empty()){
-//                 int node = q.front();
-//                 q.pop();
-//                 bfs.push_back(node);
-//                 for(auto it : adjList[node]){
-//                     if(!vis[it]){
-//                         vis[it] = 1;
-//                         q.push(it);
-//                     }
-//                 }
-//             }
-//             return bfs;
-//         }
-// };
-
-// int main() {
-//     cout << "start";
-//     Solution object;
-//     vector<pair<int,int>> adj = {{0,1}, {1,2}, {2,3}, {3,4}, {4,0}};
-
-//     vector<int> ans = object.bfsGraphTraversal(5, adj);
-
-//     for(auto i : ans) cout << i << " ";
-//     return 0;
-// }
-
 class Solution {
-  public:
-    // Function to return Breadth First Traversal of given graph.
-    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
-        int vis[V] = {0}; 
-        vis[0] = 1; 
-        queue<int> q;
-        // push the initial starting node 
-        q.push(0); 
-        vector<int> bfs; 
-        // iterate till the queue is empty 
-        while(!q.empty()) {
-           // get the topmost element in the queue 
-            int node = q.front(); 
-            q.pop(); 
-            bfs.push_back(node); 
-            // traverse for all its neighbours 
-            for(auto it : adj[node]) {
-                // if the neighbour has previously not been visited, 
-                // store in Q and mark as visited 
-                if(!vis[it]) {
-                    vis[it] = 1; 
-                    q.push(it); 
+    public : 
+        vector<int> bfsGraphTraversal(int V, vector<int> adj[]){
+            vector<int> vis(V,0);
+            vis[0] = 1;
+            queue<int> q;
+            q.push(0);
+            vector<int> bfs;
+
+            while(!q.empty()){
+                int node = q.front();
+                q.pop();
+                bfs.push_back(node);
+                for(auto it : adj[node]){
+                    if(!vis[it]){
+                        vis[it] = 1;
+                        q.push(it);
+                    }
                 }
             }
+            return bfs;
         }
-        return bfs; 
-    }
 };
 
 void addEdge(vector <int> adj[], int u, int v) {
@@ -94,7 +41,7 @@ void printAns(vector <int> &ans) {
 
 int main() 
 {
-    vector <int> adj[6];
+    vector <int> adj[5];
     
     addEdge(adj, 0, 1);
     addEdge(adj, 1, 2);
@@ -102,7 +49,7 @@ int main()
     addEdge(adj, 0, 4);
 
     Solution obj;
-    vector <int> ans = obj.bfsOfGraph(5, adj);
+    vector <int> ans = obj.bfsGraphTraversal(5, adj);
     printAns(ans);
 
     return 0;
